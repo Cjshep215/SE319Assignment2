4ecbb351011badd08484a8e6ae3abeebe642b0db
Luke Herbsleb <luke@herbsleb.com>
2024-04-05 18:54:34 -0500

    added the payment part of view 2 and receipt on screen 3 (not much functionality)

--- assignment2/src/script.js
+++ assignment2/src/script.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import items from "./data.json";
+import { useForm } from "react-hook-form";
 
 export function App() {
   const [oneView, setOneView] = useState(true);
@@ -18,12 +19,20 @@ export function App() {
     setCart([...cart, el]);
   };
 
+  //   const function and payment are copied from assignment 12, delete as needed
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const [dataF, setDataF] = useState({});
+  const [viewer, setViewer] = useState(0);
+
   // <button type="button" onClick={() => removeFromCart(el)}> - </button>{" "}
   // <button type="button" variant="light" onClick={() => addToCart(el)}> +</button>
 
   //bootstrap album
   function View1() {
-
     return (
       <main>
         <div className="album py-5 bg-body-tertiary">
@@ -40,7 +49,7 @@ export function App() {
                         <button
                           type="button"
                           className="btn btn-sm btn-outline-secondary"
-                        //   onClick={() => addToCart(el)}
+                          //   onClick={() => addToCart(el)}
                         >
                           +
                         </button>
@@ -216,55 +225,141 @@ export function App() {
       </div>
     ));
 
-      return (
-        <div>
-          <div>
-            <button onClick={setViewOne}>Back to Store</button>
-          </div>
-          Gaffer's Guild STORE (SE/ComS319 Assignment 2)
-          <div className="card">
-            <div className="row">
-              {/* HERE, IT IS THE SHOPING CART */}
-              <div className="col-md-8 cart">
-                <div className="title">
-                  <div className="row">
-                    <div className="col">
-                      <h4>
-                        <b>Cart:</b>
-                      </h4>
-                    </div>
-                    <div className="col align-self-center text-right text-muted">
-                      Products selected {cart.length}
-                    </div>
-                  </div>
-                </div>
-                {/* <div>{listItems}</div> */}
-              </div>
-              <div className="float-end">
-                <p className="mb-0 me-5 d-flex align-items-center">
-                  <span className="small text-muted me-2">Order total:</span>
-                  <span className="lead fw-normal">${cartTotal}</span>
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="card">
-            <h1>Payment Information</h1>
-          </div>
-          {/* <button onClick={setViewThree}>Checkout</button> */}
-        </div>
-      );
-    }
-  //   //list of purchased items
-  function View3() {
+    const onSubmit = (data) => {
+        console.log(data); // log all data
+        console.log(data.fullName); // log only fullname
+        // update hooks
+        setDataF(data);
+        setViewer(1);
+      };
+
     return (
       <div>
-        <h1>This is View 3</h1>
-        <img
-          src="https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg"
-          width={200}
-          alt="view3"
-        />
+        <div>
+          <button onClick={setViewOne}>Back to Store</button>
+        </div>
+        Gaffer's Guild STORE (SE/ComS319 Assignment 2)
+        <div className="card">
+          <div className="row">
+            {/* HERE, IT IS THE SHOPING CART */}
+            <div className="col-md-8 cart">
+              <div className="title">
+                <div className="row">
+                  <div className="col">
+                    <h4>
+                      <b>Cart:</b>
+                    </h4>
+                  </div>
+                  <div className="col align-self-center text-right text-muted">
+                    Products selected {cart.length}
+                  </div>
+                </div>
+              </div>
+              {/* <div>{listItems}</div> */}
+            </div>
+            <div className="float-end">
+              <p className="mb-0 me-5 d-flex align-items-center">
+                <span className="small text-muted me-2">Order total:</span>
+                <span className="lead fw-normal">${cartTotal}</span>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div>
+            <form onSubmit={handleSubmit(onSubmit)} className="container mt-5">
+              <div className="form-group">
+                <div className="form-group">
+                  <input
+                    {...register("fullName", { required: true })}
+                    placeholder="Full Name"
+                    className="form-control"
+                  />
+                  {errors.fullName && (
+                    <p className="text-danger">Full Name is required.</p>
+                  )}
+                </div>
+                <input
+                  {...register("email", {
+                    required: true,
+                    pattern: /^\S+@\S+$/i,
+                  })}
+                  placeholder="Email"
+                  className="form-control"
+                />
+                {errors.email && (
+                  <p className="text-danger">Email is required.</p>
+                )}
+                <input
+                  {...register("creditCard", { required: true })}
+                  placeholder="Credit Card"
+                  className="form-control"
+                />
+                {errors.creditCard && (
+                  <p className="text-danger">Credit Card is required.</p>
+                )}
+                <input
+                  {...register("address", { required: true })}
+                  placeholder="Address"
+                  className="form-control"
+                />
+                {errors.address && <p>Address is required.</p>}
+                <input {...register("address2")} placeholder="Address 2" />
+                <input
+                  {...register("city", { required: true })}
+                  placeholder="City"
+                  // className="form-control"
+                />
+                {errors.city && <p>City is required.</p>}
+                <input
+                  {...register("state", { required: true })}
+                  placeholder="State"
+                  className="form-control"
+                />
+                {errors.state && (
+                  <p className="text-danger">State is required.</p>
+                )}
+                <input
+                  {...register("zip", { required: true })}
+                  placeholder="Zip"
+                  className="form-control"
+                />
+                {errors.zip && <p>Zip is required.</p>}
+                <button type="submit" className="btn btn-primary">
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+        {/* <button onClick={setViewThree}>Checkout</button> */}
+      </div>
+    );
+  }
+  //   //list of purchased items
+  function View3() {
+    
+
+    const updateHooks = () => {
+      setViewer(0);
+      setDataF({});
+    };
+
+    return (
+      <div>
+        <h1>Payment summary:</h1>
+        <h3>{dataF.fullName}</h3>
+        <p>{dataF.email}</p>
+        <p>{dataF.creditCard}</p>
+        <p>{dataF.address}</p>
+        {/* <p>{dataF.address2}</p> */}
+        <p>
+          {dataF.city},{dataF.state} {dataF.zip}
+        </p>
+
+        <button onClick={updateHooks} className="btn btn-secondary">
+          Submit
+        </button>
       </div>
     );
   }
