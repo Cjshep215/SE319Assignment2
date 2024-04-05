2465a6ef221c2472cc5e09c7b0cc91b4f6d82b9f
Luke Herbsleb <luke@herbsleb.com>
2024-04-05 16:39:52 -0500

    working on view 1

--- assignment2/src/script.js
+++ assignment2/src/script.js
@@ -1,64 +1,217 @@
 import React, { useState, useEffect } from "react";
 
-export function App (){
+export function App() {
+  //   const [oneView, setOneView] = useState(false);
+  //   const [secondView, setSecondView] = useState(false);
+  //   const [thirdView, setThirdView] = useState(false);
 
-    const [oneView, setOneView] = useState(false);
-    const [secondView, setSecondView] = useState(false);
-    const [thirdView, setThirdView] = useState(false);
+  //bootstrap album
+  function View1() {
+    return (
+      <main>
+        <div className="album py-5 bg-body-tertiary">
+          <div className="container">
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+              <div className="col">
+                <div className="card shadow-sm">
+                  <svg
+                    className="bd-placeholder-img card-img-top"
+                    width="100%"
+                    height="225"
+                    xmlns="http://www.w3.org/2000/svg"
+                    role="img"
+                    aria-label="Placeholder: Thumbnail"
+                    preserveAspectRatio="xMidYMid slice"
+                    focusable="false"
+                  >
+                    <title>Placeholder</title>
+                    <rect width="100%" height="100%" fill="#55595c" />
+                    <text x="50%" y="50%" fill="#eceeef" dy=".3em">
+                      Thumbnail
+                    </text>
+                  </svg>
+                  <div className="card-body">
+                    <p className="card-text">
+                      This is a wider card with supporting text below as a
+                      natural lead-in to additional content. This content is a
+                      little bit longer.
+                    </p>
+                    <div className="d-flex justify-content-between align-items-center">
+                      <div className="btn-group">
+                        <button
+                          type="button"
+                          className="btn btn-sm btn-outline-secondary"
+                        >
+                          View
+                        </button>
+                        <button
+                          type="button"
+                          className="btn btn-sm btn-outline-secondary"
+                        >
+                          Edit
+                        </button>
+                      </div>
+                      <small className="text-body-secondary">9 mins</small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col">
+                <div className="card shadow-sm">
+                  <svg
+                    className="bd-placeholder-img card-img-top"
+                    width="100%"
+                    height="225"
+                    xmlns="http://www.w3.org/2000/svg"
+                    role="img"
+                    aria-label="Placeholder: Thumbnail"
+                    preserveAspectRatio="xMidYMid slice"
+                    focusable="false"
+                  >
+                    <title>Placeholder</title>
+                    <rect width="100%" height="100%" fill="#55595c" />
+                    <text x="50%" y="50%" fill="#eceeef" dy=".3em">
+                      Thumbnail
+                    </text>
+                  </svg>
+                  <div className="card-body">
+                    <p className="card-text">
+                      This is a wider card with supporting text below as a
+                      natural lead-in to additional content. This content is a
+                      little bit longer.
+                    </p>
+                    <div className="d-flex justify-content-between align-items-center">
+                      <div className="btn-group">
+                        <button
+                          type="button"
+                          className="btn btn-sm btn-outline-secondary"
+                        >
+                          View
+                        </button>
+                        <button
+                          type="button"
+                          className="btn btn-sm btn-outline-secondary"
+                        >
+                          Edit
+                        </button>
+                      </div>
+                      <small className="text-body-secondary">9 mins</small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col">
+                <div className="card shadow-sm">
+                  <svg
+                    className="bd-placeholder-img card-img-top"
+                    width="100%"
+                    height="225"
+                    xmlns="http://www.w3.org/2000/svg"
+                    role="img"
+                    aria-label="Placeholder: Thumbnail"
+                    preserveAspectRatio="xMidYMid slice"
+                    focusable="false"
+                  >
+                    <title>Placeholder</title>
+                    <rect width="100%" height="100%" fill="#55595c" />
+                    <text x="50%" y="50%" fill="#eceeef" dy=".3em">
+                      Thumbnail
+                    </text>
+                  </svg>
+                  <div className="card-body">
+                    <p className="card-text">
+                      This is a wider card with supporting text below as a
+                      natural lead-in to additional content. This content is a
+                      little bit longer.
+                    </p>
+                    <div className="d-flex justify-content-between align-items-center">
+                      <div className="btn-group">
+                        <button
+                          type="button"
+                          className="btn btn-sm btn-outline-secondary"
+                        >
+                          View
+                        </button>
+                        <button
+                          type="button"
+                          className="btn btn-sm btn-outline-secondary"
+                        >
+                          Edit
+                        </button>
+                      </div>
+                      <small className="text-body-secondary">9 mins</small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    );
+  }
+  //   //take from in-className (checkout screen)
+  //   function View2() {
+  //     return (
+  //       <div>
+  //         <h1>This is View 2</h1>
+  //         <img
+  //           src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
+  //           width={200}
+  //           alt="view2"
+  //         />
+  //       </div>
+  //     );
+  //   }
+  //   //list of purchased items
+  //   function View3() {
+  //     return (
+  //       <div>
+  //         <h1>This is View 3</h1>
+  //         <img
+  //           src="https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg"
+  //           width={200}
+  //           alt="view3"
+  //         />
+  //       </div>
+  //     );
+  //   }
 
-    function View1() {
-        return(<div>
-            <h1>This is View 1</h1>
-            <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" width={200} alt="view1" />
-        </div>);
-    }
+  //   const setViewOne = () => {
+  //     if (oneView === false) {
+  //       setOneView(true);
+  //     } else {
+  //       setOneView(false);
+  //     }
+  //   };
 
-    function View2() {
-        return(<div>
-            <h1>This is View 2</h1>
-            <img src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg" width={200} alt="view2" />
-        </div>);
-    }
+  //   const setViewTwo = () => {
+  //     if (secondView === false) {
+  //       setSecondView(true);
+  //     } else {
+  //       setSecondView(false);
+  //     }
+  //   };
 
-    function View3() {
-        return(<div>
-            <h1>This is View 3</h1>
-            <img src="https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg" width={200} alt="view3" />
-        </div>);
-    }
+  //   const setViewThree = () => {
+  //     if (thirdView === false) {
+  //       setThirdView(true);
+  //     } else {
+  //       setThirdView(false);
+  //     }
+  //   };
 
-    const setViewOne = () => {
-        if (oneView === false) {
-            setOneView(true);
-        } else {
-            setOneView(false);
-        }
-    }
-
-    const setViewTwo = () => {
-        if (secondView === false) {
-            setSecondView(true);
-        } else {
-            setSecondView(false);
-        }
-    }
-
-    const setViewThree = () => {
-        if (thirdView === false) {
-            setThirdView(true);
-        } else {
-            setThirdView(false);
-        }
-    }
-
-
-    return(<div>
-        <button onClick ={setViewOne}>One</button>
-        <button onClick ={setViewTwo}>Two</button>
-        <button onClick ={setViewThree}>Three</button>
-        {oneView && <View1 />}
-        {secondView && <View2 />}
-        {thirdView && <View3 />}
-    </div>);
-
+  return (
+    <div>
+      {/* <button onClick={setViewOne}>One</button>
+      <button onClick={setViewTwo}>Two</button>
+      <button onClick={setViewThree}>Three</button> */}
+      {/* {oneView && <View1 />} */}
+      <View1 />
+      {/* {secondView && <View2 />}
+      {thirdView && <View3 />} */}
+    </div>
+  );
 }
+
+export default App;
