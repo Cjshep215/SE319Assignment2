4c8e0c4a5c3f5baac33012fdbe466c7bd5b0c7d2
Luke Herbsleb <luke@herbsleb.com>
2024-04-05 12:29:35 -0500

    changed app.js to script.js to fit assignment reccomendations

--- assignment2/src/App.js
+++
@@ -1,7 +0,0 @@
-
-
-function App() {
-  return <div> This is app.js</div>;
-}
-
-export default App;
---
+++ assignment2/src/script.js
@@ -0,0 +1,7 @@
+
+//replace this file with three views assignment then build from there?
+function App() {
+  return <div> This is script.js</div>;
+}
+
+export default App;
