7c81ee5c81733cbc567d78c67a46ad1549b2b288
Luke Herbsleb <luke@herbsleb.com>
2024-04-05 16:11:09 -0500

    added three views

--- assignment2/src/script.js
+++ assignment2/src/script.js
@@ -1,7 +1,64 @@
+import React, { useState, useEffect } from "react";
 
-//replace this file with three views assignment then build from there?
-function App() {
-  return <div> This is script.js</div>;
+export function  Threeviews (){
+
+    const [oneView, setOneView] = useState(false);
+    const [secondView, setSecondView] = useState(false);
+    const [thirdView, setThirdView] = useState(false);
+
+    function View1() {
+        return(<div>
+            <h1>This is View 1</h1>
+            <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" width={200} alt="view1" />
+        </div>);
+    }
+
+    function View2() {
+        return(<div>
+            <h1>This is View 2</h1>
+            <img src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg" width={200} alt="view2" />
+        </div>);
+    }
+
+    function View3() {
+        return(<div>
+            <h1>This is View 3</h1>
+            <img src="https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg" width={200} alt="view3" />
+        </div>);
+    }
+
+    const setViewOne = () => {
+        if (oneView === false) {
+            setOneView(true);
+        } else {
+            setOneView(false);
+        }
+    }
+
+    const setViewTwo = () => {
+        if (secondView === false) {
+            setSecondView(true);
+        } else {
+            setSecondView(false);
+        }
+    }
+
+    const setViewThree = () => {
+        if (thirdView === false) {
+            setThirdView(true);
+        } else {
+            setThirdView(false);
+        }
+    }
+
+
+    return(<div>
+        <button onClick ={setViewOne}>One</button>
+        <button onClick ={setViewTwo}>Two</button>
+        <button onClick ={setViewThree}>Three</button>
+        {oneView && <View1 />}
+        {secondView && <View2 />}
+        {thirdView && <View3 />}
+    </div>);
+
 }
-
-export default App;
